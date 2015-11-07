4403f304b4bfd5ce36c4abb6cf608fac336f1ae8
blakejkaplan <blakejkaplan@gmail.com>
2015-11-07 17:26:21 -0500

    Changed Xcode project configuration settings

--- TutorMeTutorU/TutorMeTutorU/ViewController.swift
+++ TutorMeTutorU/TutorMeTutorU/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        var ref = Firebase(url:"https://tutormetutoru.firebaseio.com")
         
+        var alanisawesome = ["full_name": "Alan Turing", "date_of_birth": "June 23, 1912"]
+        var gracehop = ["full_name": "Grace Hopper", "date_of_birth": "December 9, 1906"]
+        
+        var usersRef = ref.childByAppendingPath("users")
+        
+        var users = ["alanisawesome": alanisawesome, "gracehop": gracehop]
+        usersRef.setValue(users)
     }
 
     override func didReceiveMemoryWarning() {
