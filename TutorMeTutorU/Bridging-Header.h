c1a443f3c35f375d234c8fb35a87cdb478879e4a
blakejkaplan <blakejkaplan@gmail.com>
2015-11-07 16:57:11 -0500

    Integrated Firebase

---
+++ TutorMeTutorU/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  TutorMeTutorU
+//
+//  Created by Blake Kaplan on 11/7/15.
+//  Copyright © 2015 Blake Kaplan. All rights reserved.
+//
+
+#import <Firebase/Firebase.h>
---
+++ TutorMeTutorU/Pods/Target Support Files/Pods-TutorMeTutorU/Pods-TutorMeTutorU-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TutorMeTutorU : NSObject
+@end
+@implementation PodsDummy_Pods_TutorMeTutorU
+@end
